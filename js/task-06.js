9c98818b5ca91dc7435664aeca50670bb3d11022
KizimJule <kizimjule@gmail.com>
2022-08-19 18:15:44 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,9 @@ const inputText = document.querySelector(`#validation-input`);
 inputText.addEventListener(`blur`, checkTextInput);
 
 function checkTextInput(event) {
-  // console.log(typeof event.currentTarget.value.length);
-  if (event.currentTarget.value.length === Number(inputText.getAttribute(`data-length`))) {
+  let textValue = event.currentTarget.value.trim();
+  if (textValue.length === Number(inputText.getAttribute(`data-length`))) {
     inputText.classList.add(`valid`);
     inputText.classList.remove(`invalid`);
-    // if (inputText.classList.contains(`invalid`)) {
-    //   inputText.classList.remove(`invalid`);
-    // }
   } else inputText.classList.add(`invalid`);
 }
-// console.log(typeof inputText.getAttribute(`data-length`));
